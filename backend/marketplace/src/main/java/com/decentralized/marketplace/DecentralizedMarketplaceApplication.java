054c4045b74d1e72638ff940072ef56ecc36fa24
Nanigopal Rana <nanigopalr972@gmail.com>
2025-04-15 08:47:08 +0530

    project setup

---
+++ backend/marketplace/src/main/java/com/decentralized/marketplace/DecentralizedMarketplaceApplication.java
@@ -0,0 +1,13 @@
+package com.decentralized.marketplace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DecentralizedMarketplaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DecentralizedMarketplaceApplication.class, args);
+	}
+
+}
---
+++ backend/marketplace/src/test/java/com/decentralized/marketplace/DecentralizedMarketplaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.decentralized.marketplace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DecentralizedMarketplaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
