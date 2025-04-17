172377ac0e81223b41cd4f90180fbc0b6c965449
Nanigopal Rana <nanigopalr972@gmail.com>
2025-04-17 12:40:44 +0530

    remove application.properties file

--- backend/marketplace/src/main/java/com/decentralized/marketplace/contract/service/ContractService.java
+++ backend/marketplace/src/main/java/com/decentralized/marketplace/contract/service/ContractService.java
@@ -11,8 +11,8 @@ import org.web3j.protocol.Web3j;
 import org.web3j.protocol.core.methods.response.TransactionReceipt;
 import org.web3j.protocol.http.HttpService;
 import org.web3j.tuples.generated.Tuple2;
-import org.web3j.tx.gas.DefaultGasProvider;
 import org.web3j.tx.gas.StaticGasProvider;
+import org.web3j.utils.Convert;
 
 import java.math.BigInteger;
 
@@ -20,8 +20,7 @@ import java.math.BigInteger;
 @Service
 public class ContractService {
 
-    private  Web3j web3j;
-    private  Demo demo;
+    private Demo demo;
     @Value("${web3.rpc.url}")
     private String rpcUrl;
 
@@ -33,28 +32,32 @@ public class ContractService {
 
     @PostConstruct
     public void init() {
-        this.web3j = Web3j.build(new HttpService(rpcUrl));
+        Web3j web3j = Web3j.build(new HttpService(rpcUrl));
         Credentials credentials = Credentials.create(privateKey);
-        BigInteger gasLimit = BigInteger.valueOf(5_000_000);         // safe for Ganache
-        BigInteger gasPrice = BigInteger.valueOf(20_000_000_000L);   // 20 Gwei
-
-        this.demo = Demo.load(
+//        BigInteger gasLimit = BigInteger.valueOf(5_000_000);         // safe for Ganache
+//        BigInteger gasPrice = BigInteger.valueOf(20_000_000_000L);   // 20 Gwei
+            this.demo = Demo.load(
                 contractAddress,
-                web3j,
-                credentials,
-                new StaticGasProvider(gasPrice, gasLimit)
-        );
+                    web3j,
+                    credentials,
+                    new StaticGasProvider(
+                            Convert.toWei("20", Convert.Unit.GWEI).toBigInteger(),
+                            BigInteger.valueOf(5_000_000)
+                    )
+            );
+            log.info("credential.getAddress() = {}", credentials.getAddress());
+            log.info("demo.getContractAddress() = {}", demo.getContractAddress());
     }
 
     public Tuple2<String, BigInteger> getUserFromBlockchain(String userAddr) throws Exception {
         Tuple2<String, BigInteger> send = demo.getUser(userAddr).send();
-        log.info("User from blockchain: {}, {}", send.component1(),send.component2());
+        log.info("User from blockchain: {}, {}", send.component1(), send.component2());
         return send;
     }
 
 
     public void setUserFromBlockchain(DemoDTO dto) throws Exception {
         TransactionReceipt send = demo.setUser(dto.getName(), BigInteger.valueOf(dto.getAge())).send();// replace with your function
-        log.info("User set from blockchain: {}",send.toString());
+        log.info("User set from blockchain: {}", send.toString());
     }
 }
